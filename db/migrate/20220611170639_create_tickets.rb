4317427371c424213cb04be503966cfc2ae9a5b8
mattriney <61891660+mattriney@users.noreply.github.com>
2022-06-11 13:03:33 -0500

    Update 20220611170639_create_tickets.rb
    Add references to tickets migration file, minus creator ref(user mask)

--- db/migrate/20220611170639_create_tickets.rb
+++ db/migrate/20220611170639_create_tickets.rb
@@ -4,6 +4,9 @@ class CreateTickets < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :friendly_ticket_id
       t.string :description
+      t.references :organization, null: false, foreign_key: true
+      t.references :client, null: false, foreign_key: true
+      t.references :priority, null: false, foreign_key: true
 
       t.timestamps
     end
