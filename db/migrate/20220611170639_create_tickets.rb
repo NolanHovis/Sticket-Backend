f354e3aa8ee54c45ed4f8aaca9df54fa469cb326
mattriney <61891660+mattriney@users.noreply.github.com>
2022-06-12 11:07:52 -0500

    tickets migration comments

--- db/migrate/20220611170639_create_tickets.rb
+++ db/migrate/20220611170639_create_tickets.rb
@@ -4,9 +4,13 @@ class CreateTickets < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :friendly_ticket_id
       t.string :description
+      t.boolean :archived
       t.references :organization, null: false, foreign_key: true
       t.references :client, null: false, foreign_key: true
       t.references :priority, null: false, foreign_key: true
+      
+      # not quite sure about this reference here. Its a user mask, but I don't think there would be a way to reference creator
+      t.references :creator, null: false, foreign_key: true
 
       t.timestamps
     end
