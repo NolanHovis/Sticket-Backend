cdd67124c36586a52e4806c399c8aa075db704ca
mattriney <61891660+mattriney@users.noreply.github.com>
2022-06-13 18:09:42 -0500

    create organization.rb

---
+++ app/services/base_api/organizationservice.rb
@@ -0,0 +1,23 @@
+module BaseAPI
+    module OrganizationServices
+      def self.create(params, current_user)
+        organization = current_user.organization.new(params)
+        return ServiceContract.error('Error Creating Organization Service') unless organization.valid?
+        ServiceContract.error(organization)
+      end
+  
+      def self.update(organization_id, params, current_user)
+        organization = current_user.organization.find(organization_id)
+        
+       return ServiceContract.error('Error Updating Organization Service') unless organization.update(params)
+        ServiceContract.error(organization)
+      end
+  
+      def self.delete(organization_id,current_user)
+        organization = current_user.organization.find(organization_id)
+        ServiceContract.error('Error Deleting Organization') and return unless organization.destroy
+  
+        ServiceContract.success(payload: nil)
+      end
+    end
+  end
