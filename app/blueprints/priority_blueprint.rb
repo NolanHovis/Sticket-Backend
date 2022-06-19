7893aa64dcda2d5e21098f17f33c96aa7bda9b0f
ZeldaMazzy <hello@ezekielhickson.com>
2022-06-19 12:33:54 -0500

    slight change

--- app/blueprints/priority_blueprint.rb
+++ app/blueprints/priority_blueprint.rb
@@ -1,6 +1,6 @@
 class PriorityBlueprint < Blueprinter::Base
   identifier :id
-  field :organization
+  field :label, :sort_order, :organization
 
   view :normal do
     fields :created_at, :updated_at
