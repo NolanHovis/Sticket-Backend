4878fe4c83314a72d2b4470c4598f03952f76707
Jennifer Henson <hensonjenn_07@hotmail.com>
2022-06-20 17:59:55 -0500

    updates

--- app/blueprints/action_blueprint.rb
+++ app/blueprints/action_blueprint.rb
@@ -5,12 +5,6 @@ class ActionBlueprint < Blueprinter::Base
     identifier :id
     fields :comment, :movement, :attachment, :assignment, :priority
   
-    view :login do
-      association :token, blueprint: TokenBlueprint do |action_types_option, _options|
-        action_types_option.tokens.last
-      end
-    end
-  
     view :normal do
       fields :comment, :movement, :attachment, :assignment, :priority
     end
