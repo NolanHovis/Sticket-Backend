fc89f31c19e9ca6208aa97719453d7e7adaa263f
mattriney <61891660+mattriney@users.noreply.github.com>
2022-06-20 17:53:53 -0500

    ticket blueprint

--- app/blueprints/ticket_blueprint.rb
+++ app/blueprints/ticket_blueprint.rb
@@ -4,6 +4,8 @@
 class TicketBlueprint < Blueprinter::Base
   identifier :id
   fields :
+  view :normal do
+    fields :title, :friendly_ticket_id, :description, :archived, :organiztion_id, :client_id, :priority_id, :user_id, :created_at, :updated_at
 end
       
   end
