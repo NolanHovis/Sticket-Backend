a8ca74c060365ba806e1a2689d7ad48f46e00c9f
ZeldaMazzy <hello@ezekielhickson.com>
2022-06-20 18:19:08 -0500

    initial commit

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,76 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/users', type: :request do
+
+  path '/api/v1/users/login' do
+
+    post('login user') do
+      response(200, 'successful') do
+
+        tags 'Login User'
+        security[token: []]
+        parameter name: 
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/logout' do
+
+    delete('logout user') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/me' do
+
+    get('me user') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/users/create' do
+
+    post('create user') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
