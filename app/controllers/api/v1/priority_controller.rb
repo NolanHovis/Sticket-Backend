305a5db25b90c5410309d97376e6ed1c9b3fa5e3
Jennifer Henson <hensonjenn_07@hotmail.com>
2022-06-20 20:10:08 -0500

    updates stic-69

---
+++ app/controllers/api/v1/priority_controller.rb
@@ -0,0 +1,30 @@
+module Api
+    module V1
+  
+      def create_priority
+        priority = current_user.Priorities.new(params)
+        Priorities.save  
+      end
+  
+      def update_priority
+        priority = current_user.priorities.find(organization_id)
+        priority.update(params)
+        payload ={
+          priority: PriorityBlueprinter.render_as_hash(results.payload)
+        }
+        render_success(payload: payload)
+      end
+  
+      def delete_priority
+        priority = current_user.Priorities.find(organization_id)
+        priority.destroy
+        render_success(payload: nil)
+      end
+  
+      def list_priority
+        @priority = current_user.Priorities(organization_id)
+  
+      end
+  
+    end
+  end
