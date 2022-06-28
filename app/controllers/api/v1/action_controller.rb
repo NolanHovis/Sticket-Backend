020324fb44a954fe2c7e24a97c94371ff608d0d5
dvdllyd <dvdllyd13@gmail.com>
2022-06-27 19:37:24 -0500

    Create action_controller.rb

---
+++ app/controllers/api/v1/action_controller.rb
@@ -0,0 +1,43 @@
+module Api
+    module V1
+    
+      class ActionController < Api::V1::ApplicationController
+        
+        def create
+            result = Action.new_action(params)
+            render_error(errors: 'There was a problem creating a new action!', status: 400) and return unless result.success?
+            payload = {
+                action: ActionBlueprint.render_as_hash(result.payload, view: :normal)
+            }
+            render_success(payload: payload)
+        end
+
+        def change_status
+
+        end
+    
+        def change_priority
+
+        end                 
+                              
+        def assign
+            result = User.assign_user(params[:id], user_params, @current_user)
+            render_error(errors: 'There was a problem assigning an action!', status: 400) and return unless result.success?
+            payload = {
+               action: UserBluePrinter.render_as_hash(results.payload)
+            }
+            render_sucess(payload: payload)
+        end
+
+        private
+        def organizaion_params 
+          params.require(:action).permit(:comment, :movement, :attachment, :assignment, :priority)  
+        end
+
+
+      end
+
+
+    end      
+
+end    
