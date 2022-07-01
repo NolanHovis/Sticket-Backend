2f291801a75e11e8a7285b2626de34b4ec09b03c
Jennifer Henson <hensonjenn_07@hotmail.com>
2022-06-30 20:27:21 -0500

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,9 +32,9 @@ class User < ApplicationRecord
   scope :invite_token_is, ->(invitation_token) { where(invitation_token: invitation_token) }
 
   # Callbacks
-  before_create :generate_invitation_token
-  before_save :generate_invitation_token, if: :will_save_change_to_invitation_token?
-  after_commit :invite_user, if: :saved_change_to_invitation_token?
+  # before_create :generate_invitation_token
+  # before_save :generate_invitation_token, if: :will_save_change_to_invitation_token?
+  # after_commit :invite_user, if: :saved_change_to_invitation_token?
 
   def generate_token!(ip)
     token = Token.create(
